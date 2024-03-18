47c9149f36ad2e10856bcb7ce229aa3ea9bad758
Gabriel D'Ostuni <gabdostuni47400@gmail.com>
2024-03-18 09:09:42 +0100

    feat: pointeuse

--- config/api.lua
+++ config/api.lua
@@ -4,7 +4,6 @@ return {
 
     uris = {
         server = 'http://localhost:30120/', -- the request uri of your server
-        git = 'https://api.github.com/repos/{owner}/{repo}/releases/latest' -- the request uri of RepoVersionChecker
     },
 
     methods = {
@@ -65,6 +64,6 @@ return {
     },
 
     valideAdresses = {
-        -- ['127.0.0.1'] = true
+        ['127.0.0.1'] = true,
     }
 }
